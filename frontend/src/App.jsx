72cf8800817c2836873568143ff064fd56cc4b28
Juan Duarte <142144090+JuanDG7@users.noreply.github.com>
2025-05-22 11:09:47 -0700

    Start frontend setup - 11:08 (notebook)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home.jsx";
 import ProductsPage from "./pages/Products.jsx";
+import RootLayout from "./pages/Root.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page of our application.</p>
+      <p>
+        Go to <Link to="/products">the list of products </Link>
+      </p>
     </div>
   );
 }
